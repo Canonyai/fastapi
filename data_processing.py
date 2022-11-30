15a183969f5158ec8b2ece98f7b8676e65f7095e
MamokeArubayi <98597067+MamokeArubayi@users.noreply.github.com>
2022-11-30 15:46:05 +0000

    Add files via upload

--- data_processing.py
+++ data_processing.py
@@ -22,6 +22,23 @@ def get_code_review_time(user: Scope, repo: str):
 
     # print(*time_taken, sep="\n")
     return x_axis, y_axis
+    
+    
+# time taken for cycle time (issues start to completion) metric    
+def get_cycle_time(user: Scope, repo: str):
+    x_axis = []
+    y_axis = []
+    after = datetime.today()  # parse_date(start_date)
+    before = after - relativedelta(months=2)  # parse_date(end_date)
+    prs = user.get_issues_by_time(repo, before, after)
+    
+
+    for issue in issues:
+        x_axis.append(issues.title)
+        y_axis.append(round(user.get_time_taken(issues).seconds / 60, 3))
+
+    # print(*time_taken, sep="\n")
+    return x_axis, y_axis
 
 
 # get repositories
